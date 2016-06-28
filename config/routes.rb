2c11f9489d4a33bb1c3139808c1d86b041603e17
Gregory Covington <gc@virulantconsulting.com>
2016-06-28 16:23:12 -0500

    pages and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
   get 'pages/home'
 
   get 'pages/help'
 
+  get 'pages/about'
+
+  get 'pages/contact'
+
   get 'users/new'
-  
-root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
