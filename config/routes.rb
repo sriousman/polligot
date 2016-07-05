670d6ed623d48c82f9577d9d80100d773f943a69
Gregory Covington <gc@virulantconsulting.com>
2016-07-05 07:21:13 -0500

    Implemented basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
-  root 'pages#home'
-  get  '/help',    to: 'pages#help'
-  get  '/about',   to: 'pages#about'
-  get  '/contact', to: 'pages#contact'
-  get  '/signup',  to: 'users#new'
+  root    'pages#home'
+  get     '/help',     to: 'pages#help'
+  get     '/about',    to: 'pages#about'
+  get     '/contact',  to: 'pages#contact'
+  get     '/signup',   to: 'users#new'
+  get     '/login',    to: 'sessions#new'
+  post    '/login',    to: 'sessions#create'
+  delete  '/logout',   to: 'sessions#destroy' 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
