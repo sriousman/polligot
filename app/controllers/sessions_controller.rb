2a3d288a75f3c4a69244c51c14f6c8bcfcfef621
Gregory Covington <gc@virulantconsulting.com>
2016-07-07 11:45:36 -0500

    remember token without full testing suit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
-
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
       flash.now[:success] = "Welcome back #{user.name}!"
 
@@ -18,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
