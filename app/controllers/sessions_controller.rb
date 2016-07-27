894c112aadcddb5ed92fdb298ccf08df74b63a58
Gregory Covington <gc@virulantconsulting.com>
2016-07-27 16:24:32 -0500

    most of the way through account activation.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
-      flash.now[:success] = "Welcome back #{user.name}!"
-
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Uh Oh, something went wrong."
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
-     flash[:success] = "Welcome to Polligot #{@user.name}!"
-     log_in @user
-     redirect_to @user
+    @user.send_activation_email
+    flash[:info] = "Please check your email to activate your account."
+    redirect_to root_url
    else
      render 'new'
    end
